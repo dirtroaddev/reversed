8a4f8f87462c4dc1b1be3858bc6e57d4d281320b
fullstackjunkie <rongoodwindev@gmail.com>
2021-09-06 19:29:44 -0400

    Finished

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-const HelloWorld = () => {
-    alert("Hello, World");
+
+
+//get the values from the string
+//controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+
+//reverse the string
+//logic function
+function reverseString(userString) {
+    let revString = "";
+
+    //reverse the string using a for loop
+    for(let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+
+//display the message to the user
+//view function
+function displayString(revString) {
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
